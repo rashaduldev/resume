2dfe5a038e906309a395819e1948c52c0bda269d
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-05-18 19:57:18 +0600

    Navber Done

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"], 
@@ -18,7 +19,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header/>
+        <PageTransition>
         {children}
+        </PageTransition>
         </body>
     </html>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
+
 const Header = () => {
     return (
-        <header>
-            Header
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* logo */}
+                <Link href={'/'}>
+                    <h1 className="text-4xl font-semibold">
+                    Luke <span className="text-accent">.</span>
+                    </h1>
+                </Link>
+                {/* desktop nav amd hire me button  */}
+                <div className="hidden xl:flex items-center gap-8">
+                <Nav/>
+                 <Link href={'/contact'}>
+                 <Button>Hire me</Button>
+                 </Link>   
+                </div>
+
+                {/* Mobile nav */}
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
+            </div>
         </header>
     );
 };
