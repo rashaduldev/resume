1c7f61dca1388ab9efdfb96940f9bab8ffe1b221
rashaduldev <rashadul.dev@gmail.com>
2024-05-23 13:58:31 +0600

    Services page done

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -4,6 +4,34 @@ const Resume = () => {
     return (
         <div>
            Resume here 
+          {/* // <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+//             <div className="container mx-auto">
+//                 <motion.div
+//                 initial={{opacity:0}}
+//                 animate={{
+//                     opacity:1,
+//                     transition:{delay:2.4,duration:0.4,ease:"easeIn"}
+//                 }}
+//                 >
+//                     {
+//                         services.map((service,index)=>{
+//                             <div key={index}>
+//                                 <div>
+//                                     <div>{service.num}</div>
+//                                     <Link href={service.href}>
+//                                         <BsArrowDownRight />
+//                                     </Link>
+//                                 </div>
+//                                 <h2>{service.title}</h2>
+//                                 <p>{service.description}</p>
+//                                 <div className="border-b border-white/20 w-full" />
+//                             </div>
+
+//                         })
+//                     }
+//                 </motion.div>
+//             </div>
+//         </section> */}
         </div>
     );
 };
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -3,64 +3,64 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowDownRight } from "react-icons/bs";
 
-const services=[
-    {
-        num:"01",
-        title:"Web Development",
-        description:"Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
-        href:""
-    },
-    {
-        num:"02",
-        title:"UI/UX Design",
-        description:"Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
-        href:""
-    },
-    {
-        num:"03",
-        title:"Logo Design",
-        description:"Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
-        href:""
-    },
-    {
-        num:"04",
-        title:"SEO",
-        description:"Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
-        href:""
-    },
-]
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
+    href: ""
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
+    href: ""
+  },
+  {
+    num: "03",
+    title: "Logo Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
+    href: ""
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adip   Lorem ipsum dolor sit amet, consectetur adip",
+    href: ""
+  },
+];
 
 const Services = () => {
-    return (
-        <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
-            <div className="container mx-auto">
-                <motion.div
-                initial={{opacity:0}}
-                animate={{
-                    opacity:1,
-                    transition:{delay:2.4,duration:0.4,ease:"easeIn"}
-                }}
-                >
-                    {
-                        services.map((service,index)=>{
-                            <div key={index}>
-                                <div>
-                                    <div>{service.num}</div>
-                                    <Link href={service.href}>
-                                        <BsArrowDownRight />
-                                    </Link>
-                                </div>
-                                <h2>{service.title}</h2>
-                                <p>{service.description}</p>
-                                <div className="border-b border-white/20 w-full" />
-                            </div>
-
-                        })
-                    }
-                </motion.div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="min-h-[75vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" }
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {
+            services.map((service, index) => (
+              <div key={index} className="flex flex-1 flex-col justify-center gap-6 group">
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-semibold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</div>
+                  <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
+                <p className="text-white/60">{service.description}</p>
+                <div className="border-b border-white/20 w-full" />
+              </div>
+            ))
+          }
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
