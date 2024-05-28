7c64fac5ee53fe2000b851427aee95cf42dd002c
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2024-05-28 09:58:39 +0600

    some data change and Sweetalert implement

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,12 +6,13 @@ import { Input } from "@/components/ui/input";
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
+import Swal from "sweetalert2";
 
 const info=[
     {
         icon:<FaPhoneAlt />,
         title:"Phone",
-        description:"8801603010103"
+        description:"+8801603010103"
     },
     {
         icon:<FaEnvelope />,
@@ -26,6 +27,31 @@ const info=[
 ]
 
 const Contact = () => {
+    const handleSend = (e) => {
+        e.preventDefault();
+        const firstname = e.target.elements.firstname.value;
+        const lastname = e.target.elements.lastname.value;
+        const email = e.target.elements.email.value;
+        const phone = e.target.elements.phone.value;
+        
+        Swal.fire({
+          position: "top-middle",
+          icon: "success",
+          title: "Thank you for your message mr," +name,
+          html: `
+          <p>We will contact you very soon</p>
+          <br>
+            <p><strong>Your Sending info</strong></p>
+            <p><strong>Firstname:</strong> ${firstname}</p>
+            <p><strong>Lastname:</strong> ${lastname}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Phone:</strong> ${phone}</p>
+          `,
+          showConfirmButton: false,
+          timer: 3000,
+        });
+        e.target.reset();
+      };
     return (
         <motion.section
         initial={{ opacity: 0 }}
@@ -39,15 +65,35 @@ const Contact = () => {
             <div className="flex flex-col xl:flex-row gap-[30px]">
                 {/* form */}
                 <div className="xl:w-[54%] order-2 xl:order-none">
-                    <form className="flex flex-col gap-6 px-10 py-5 bg-[#27272c] rounded-xl">
+                    <form onSubmit={handleSend} className="flex flex-col gap-6 px-10 py-5 bg-[#27272c] rounded-xl">
                         <h3 className="text-4xl text-accent">Let's work together</h3>
-                        <p className="text-white/60">Passionate web developer skilled in HTML, CSS, JavaScript, and modern frameworks.</p>
+                        <p className="text-white/60">Passionate web developer skilled in HTML, CSS, JavaScript, and modern frameworks like Next.js, React.js, Node.js</p>
                         {/* input feilds */}
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            <Input type="firstname" placeholder="Firstname"/>
-                            <Input type="lastname" placeholder="Lastname"/>
-                            <Input type="email" placeholder="Email Address"/>
-                            <Input type="phone" placeholder="Phone Number"/>
+
+                            <Input 
+                            type="firstname" 
+                            name="firstname"
+                            required 
+                            placeholder="Firstname"/>
+
+                            <Input 
+                            type="lastname"
+                            name="lastname"
+                            required 
+                            placeholder="Lastname"/>
+
+                            <Input 
+                            type="email" 
+                            name="email"
+                            required
+                            placeholder="Email Address"/>
+
+                            <Input 
+                            type="phone" 
+                            name="phone"
+                            required
+                            placeholder="Phone Number"/>
                         </div>
                         {/* select */}
                         <Select>
